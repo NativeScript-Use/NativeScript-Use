29243c14a33684b062a8d42117ad4b063ee52970
Juan de Dios Martínez Vallejo <vallemardeveloper@gmail.com>
2023-06-13 14:05:04 +0200

    draft: native rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,15 @@
+#!/usr/bin/node --experimental-specifier-resolution=node
+
 import typescript from "rollup-plugin-ts";
+var fs = require('fs')
 import copy from "rollup-plugin-copy";
 import terser from '@rollup/plugin-terser';
 var requireFromUrl = require('require-from-url/sync');
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
-const dts = require('rollup-plugin-dts')
 const externals = ['nativescript-vue', '@nativescript/core', '@nativescript/core/application', '@nativescript/shared-notification-delegate']
-
 const tsPlugin = typescript({
     transformers: {
-      //  before: require('@nativescript/webpack/dist/transformers/NativeClass').default
         before: requireFromUrl('https://cdn.jsdelivr.net/npm/@nativescript/webpack@5.0.15/dist/transformers/NativeClass/index.js').default
     }
 });
@@ -19,38 +20,115 @@ const config = [
     {
         external: externals,
         input: "packages/core/index.ts",
-        plugins: [tsPlugin,
+        globals: {
+            './core/index.android.js': './core'
+        },
+        treeshake: false,
+        plugins: [
             copy({
                 targets: [
                     { src: 'package.json', dest: DIST_FOLDER },
-                   // { src: 'node_modules', dest: DIST_FOLDER },
+                    // { src: 'node_modules', dest: DIST_FOLDER },
                     { src: 'packages/core/platforms', dest: DIST_FOLDER }
                 ]
-            })],
+            }),
+
+            nodeResolve({
+
+                extensions: ['.tsx', '.ts', '.jsx', '.js', '.android.ts', '.ios.ts',],
+            }),
+            tsPlugin,
+
+        ],
         output: [
             {
-                file: DIST_FOLDER + "/index.js",
-                format: "es"
+                dir: DIST_FOLDER,
+                format: "es",
+                preserveModules: true,
             },
         ],
     },
     {
         external: externals,
         input: "packages/core/index.ts",
-        plugins: [tsPlugin, terser()],
+        globals: {
+            './core/index.android.js': './core'
+        },
+        treeshake: false,
+        plugins: [
+            copy({
+                targets: [
+                    { src: 'package.json', dest: DIST_FOLDER },
+                    // { src: 'node_modules', dest: DIST_FOLDER },
+                    { src: 'packages/core/platforms', dest: DIST_FOLDER }
+                ]
+            }),
+
+            nodeResolve({
+
+                extensions: ['.tsx', '.ts', '.jsx', '.js', '.ios.ts',],
+
+            }),
+            tsPlugin,
+
+            copyAndWatch('index.html', 'index.html')
+        ],
         output: [
             {
-                file: DIST_FOLDER + "/index.min.js",
-                format: "es"
+                dir: DIST_FOLDER,
+                format: "es",
+                preserveModules: true,
             },
         ],
     },
-    {
+    /* {
         external: externals,
-        input: "dist/index.d.ts",
-        output: [{ file: DIST_FOLDER + "/index.d.ts", format: "es" }],
-        plugins: [dts.default()],
-    },
+        input: "packages/core/index.ts",
+        plugins: [tsPlugin, terser()],
+        output: [
+            {
+                dir: DIST_FOLDER,
+                preserveModules: true,
+                format: "es"
+            },
+        ],
+    }, */
+    /*  {
+         external: externals,
+         input: "dist/index.d.ts",
+         output: [{
+             dir: DIST_FOLDER, format: "es",
+             preserveModules: true,
+         }],
+         plugins: [dts.default()],
+     }, */
 ];
+
+function copyAndWatch(fileIn, fileOut) {
+    return {
+        name: 'copy-and-watch',
+        async buildStart() {
+        },
+        async writeBundle(a, bundle) {
+            console.log("fileOut");
+            console.log();
+            Object.keys(bundle).forEach(fileName => {
+                fs.readFile("./publish/" + fileName, 'utf8', function (err, data) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    let result = data.replace("/index.ios.js", '');
+                    result = result.replace("/index.android.js", '');
+
+                    fs.writeFile("./publish/" + fileName, result, 'utf8', function (err) {
+                        if (err) return console.log(err);
+                    });
+                });
+
+            })
+
+        }
+    }
+}
 
 export default config;
