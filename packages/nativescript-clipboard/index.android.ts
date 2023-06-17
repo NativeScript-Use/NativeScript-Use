f73ebf669663e48603127e68f5f38c4cc448c518
Juan de Dios Martínez Vallejo <vallemardeveloper@gmail.com>
2023-06-17 11:39:04 +0200

    fix(clipboard): null getPrimaryClip

--- packages/nativescript-clipboard/index.android.ts
+++ packages/nativescript-clipboard/index.android.ts
@@ -1,47 +1,43 @@
-import { Utils } from "@nativescript/core";
-import { ClipboardBase } from "./common";
+import { Utils } from '@nativescript/core';
+import { ClipboardBase } from './common';
 
 export class Clipboard extends ClipboardBase {
+  protected clipboard: android.content.ClipboardManager;
 
-    protected clipboard: android.content.ClipboardManager;
+  constructor() {
+    super();
+    this.clipboard = Utils.android.getApplicationContext().getSystemService(android.content.Context.CLIPBOARD_SERVICE);
+  }
 
-    constructor() {
-        super();
-        this.clipboard = Utils.android.getApplicationContext().getSystemService(android.content.Context.CLIPBOARD_SERVICE);
-    }
+  onCopy(callback: (text: string) => void): void {
+    this.offCopy();
+    let THRESHOLD_MS = 50;
+    let enableCallback = true;
+    this.listener = new android.content.ClipboardManager.OnPrimaryClipChangedListener({
+      onPrimaryClipChanged: () => {
+        if (enableCallback) callback(this.clipboard.getPrimaryClip()?.getItemAt(0)?.getText()?.toString());
+        enableCallback = false;
+        setTimeout(() => {
+          enableCallback = true;
+        }, THRESHOLD_MS);
+      },
+    });
+    this.clipboard.addPrimaryClipChangedListener(this.listener);
+  }
 
-    onCopy(callback: (text: string) => void): void {
-        this.offCopy();
-        let THRESHOLD_MS = 50;
-        let enableCallback = true;
-        this.listener = new android.content.ClipboardManager.OnPrimaryClipChangedListener({
-            onPrimaryClipChanged: () => {
-                if (enableCallback)
-                    callback(this.clipboard.getPrimaryClip().getItemAt(0)?.getText()?.toString());
-                enableCallback = false;
-                setTimeout(() => {
-                    enableCallback = true;
-                }, THRESHOLD_MS);
-            },
-        });
-        this.clipboard.addPrimaryClipChangedListener(this.listener)
-    }
+  offCopy(): void {
+    if (this.listener) this.clipboard.removePrimaryClipChangedListener(this.listener);
+  }
 
-    offCopy(): void {
-        if (this.listener)
-            this.clipboard.removePrimaryClipChangedListener(this.listener)
-    }
+  copy(text: string): boolean {
+    const clip = android.content.ClipData.newPlainText('App clipboard data', text);
+    this.clipboard.setPrimaryClip(clip);
+    return true;
+  }
 
-    copy(text: string): boolean {
-        const clip = android.content.ClipData.newPlainText("App clipboard data", text);
-        this.clipboard.setPrimaryClip(clip);
-        return true
-    }
-
-    read(): string {
-        const item = this.clipboard.getPrimaryClip().getItemAt(0);
-        let content = item.getText().toString();
-        return content || "";
-    }
-
+  read(): string {
+    const item = this.clipboard.getPrimaryClip()?.getItemAt(0);
+    let content = item.getText().toString();
+    return content || '';
+  }
 }
