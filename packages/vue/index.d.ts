4c0bd814dbd50ec7a781c66ac85999d0d8dd1014
Juan de Dios Martínez Vallejo <vallemardeveloper@gmail.com>
2023-06-18 19:35:22 +0200

    fix: exports

--- packages/vue/index.d.ts
+++ packages/vue/index.d.ts
@@ -1,3 +1,4 @@
+/* Functions */
 export * from './src/unrefView';
 export * from './src/useClipboard';
 export * from './src/useColorMode';
@@ -5,8 +6,11 @@ export * from './src/useColorPalette';
 export * from './src/useElementSize';
 export * from './src/useEventListener';
 export * from './src/useKeyboard';
+export * from './src/useMediaQuery';
 export * from './src/useRootLayout';
 export * from './src/useScreenOrientation';
 export * from './src/useStorage';
 export * from './src/types';
+
+/* Global hooks */
 export * from './src/onApplicationMounted';
