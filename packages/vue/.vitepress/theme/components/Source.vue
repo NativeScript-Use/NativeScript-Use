0ddbd0a6841068f6a54e4ec6fb6eebadfd6bc813
Juan de Dios Martínez Vallejo <vallemardeveloper@gmail.com>
2023-09-17 16:56:02 +0200

    chore(vue/doc): fix gitignore

---
+++ packages/vue/.vitepress/theme/components/Source.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex items-center justify-start">
+    <a target="_blank" class="cursor-pointer" :href="sourceURL">Source</a>
+    <div v-if="demoURL">
+      <span class="mx-2">•</span>
+      <a target="_blank" class=" cursor-pointer" :href="demoURL">Demo</a>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const { source, demo } = defineProps({
+  source: String, demo: String
+})
+
+//@ts-ignore
+const sourceURL = BASE_BLOB_SOURCE + source + "/index.ts";
+//@ts-ignore
+const demoURL = demo ? STACKBLITZ_VIEW + demo : null;
+
+</script>
