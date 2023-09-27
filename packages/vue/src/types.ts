98c710611ff9dd896ff8b7a62c35a41a44e484c4
Juan de Dios Martínez Vallejo <vallemardeveloper@gmail.com>
2023-09-27 23:30:18 +0200

    feat(vue): extract view into unrefView when it is a view

--- packages/vue/src/types.ts
+++ packages/vue/src/types.ts
@@ -1,5 +1,4 @@
 import { View } from '@nativescript/core';
 import { Ref } from 'nativescript-vue';
 
-//TODO: Refactor: export type ViewRef<T = View> = Ref<T | { nativeView?: T } | { $el?: { nativeView?: T } }>;
-export type ViewRef<T = View> = Ref<T & { nativeView?: T } & { $el?: { nativeView?: T } }> | Ref<{ nativeView?: T } & T & { $el?: { nativeView?: T } }> | Ref<{ $el?: { nativeView?: T } } & T & { nativeView?: T }>;
+export type ViewRef<T = View> = T | Ref<T | { nativeView?: T; $el?: { nativeView: T } }>;
--- packages/vue/src/unrefView/index.ts
+++ packages/vue/src/unrefView/index.ts
@@ -6,6 +6,11 @@ import { ViewRef } from '../types';
  *
  * @param target
  */
-export function unrefView<T extends View = View>(ref: ViewRef<T>): T | undefined {
-  return ref?.value instanceof View ? ref?.value : ref?.value?.$el?.nativeView ?? ref?.value?.nativeView ?? undefined;
+export function unrefView<T extends View = View>(ref: T | ViewRef<T>): T | undefined {
+  if (ref instanceof View) return ref;
+  if (ref?.value instanceof View) return ref?.value;
+  if (ref?.value?.nativeView) return ref?.value.nativeView;
+  if (ref?.value?.$el?.nativeView) return ref?.value?.$el?.nativeView;
+
+  return undefined;
 }
