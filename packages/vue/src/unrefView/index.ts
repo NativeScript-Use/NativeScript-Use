6f4c1e34baa47ac860c772c1225ab1eb2839ff31
Juan de Dios Martínez Vallejo <vallemardeveloper@gmail.com>
2023-11-18 04:12:49 +0100

    release(vue): 0.0.33 - unrefView check if view is NSView

--- packages/vue/src/unrefView/index.ts
+++ packages/vue/src/unrefView/index.ts
@@ -7,12 +7,12 @@ import { ViewRef } from '../types';
  * @param target
  */
 export function unrefView<T = View>(ref: T | any /*  ViewRef<T> */): T | undefined {
+  if (ref?.value instanceof View) return ref?.value;
+  if (ref instanceof View) return ref as T;
   if (ref?.value?.nativeView) return ref?.value.nativeView;
   if (ref?.value?.$el?.nativeView) return ref?.value?.$el?.nativeView;
   if (ref?.nativeView) return ref?.nativeView;
   if (ref?.$el?.nativeView) return ref?.$el?.nativeView;
-  if (ref?.value instanceof View) return ref?.value;
-  if (ref instanceof View) return ref as T;
   // @ts-ignore
   if (Array.isArray(ref?.value)) return (ref?.value as any[]).map((view) => unrefView(view)) as T[];
   // @ts-ignore
